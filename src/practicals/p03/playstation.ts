a714ede0433166e2a74849f0f5b5b6ce44b81afb
NUTTOD <nuttod88@gmail.com>
2026-02-16 09:59:31 +0700

    p03-1

--- src/practicals/p03/playstation.ts
+++ src/practicals/p03/playstation.ts
@@ -2,4 +2,18 @@ import { Product } from './product'
 
 export class Playstation extends Product {
   // TODO: implement class properties, constructor with super(...), and methods
+  private generation:number = 0;
+
+  constructor(name:string, generation:number, price:number){
+    super(name, price)
+    this.generation = generation
+  }
+
+  getProfile():string{
+    return `${this.name} (Gen ${this.generation})`
+  }
+
+  getDiscountPrice():number{
+    return this.price - (this.price * 10/100)
+  }
 }
--- src/practicals/p03/product.ts
+++ src/practicals/p03/product.ts
@@ -1,2 +1,19 @@
-export class Product {}
+export class Product {
+    public readonly name:string = ""
+    protected price:number = 0
+    public static readonly DISCOUNT_PERCENT:number = 10;
+
+    constructor(name:string, price:number = 0){
+        this.name = name
+        this.price = price
+    }
+
+    public setPrice(price:number):void {
+        this.price = price
+    }
+
+    public getPrice():number {
+        return this.price
+    }
+}
 
