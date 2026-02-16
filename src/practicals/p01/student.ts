27c61503091b9d4e872eb7ec63b8b11f981e3417
NUTTOD <nuttod88@gmail.com>
2026-02-16 09:19:00 +0700

    p01-1

--- src/practicals/p01/student.ts
+++ src/practicals/p01/student.ts
@@ -1,1 +1,8 @@
-export class Student {}
+export class Student {
+    public firstname:string = ""
+    public lastname:string = ""
+
+    printName():void {
+        console.log(`${this.firstname} ${this.lastname}`);
+    }
+}
