77626ace7f12bda61a5b6eeb08d2512c5ef3d62d
NUTTOD <nuttod88@gmail.com>
2026-02-16 09:31:55 +0700

    p02-1

--- src/practicals/p02/person.ts
+++ src/practicals/p02/person.ts
@@ -1,1 +1,18 @@
-export class Person {}
+export class Person {
+    public firstname:string = ""
+    public lastname:string = ""
+    private Age:number = 0
+    public static readonly COUNTRY:string = "Thailand"
+
+    public setAge(age:number):void{
+        this.Age = age
+    }
+
+    public getFullName(){
+        return `${this.firstname} ${this.lastname}`;
+    }
+
+    public getAge(){
+        return this.Age
+    }
+}
